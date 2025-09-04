08a0615b1c6e7baba7d6a3e7f68f162491aff176
Vladimir Vuckovic <vladimir.vuckovic@js-soft.rs>
2025-09-04 09:35:54 +0200

    fix: services

--- srv/sales-service.cds
+++ srv/sales-service.cds
@@ -1,7 +1,19 @@
 using { sap.capire.bookshop as my } from '../db/schema';
 
 service SalesService {
-    entity SalesOrders as projection on my.SalesOrders;
-    entity SalesOrderItems as projection on my.SalesOrderItems;
-    entity Books as projection on my.Books;
+    // Main sales orders entity with proper projections
+    entity SalesOrders as projection on my.SalesOrders {
+        *  // items is already included from the schema
+    };
+
+    // Sales order items with associations
+    entity SalesOrderItems as projection on my.SalesOrderItems {
+        *  // salesOrder is already included from the schema
+    };
+
+    // Books entity for value lists and associations
+    entity Books as projection on my.Books {
+        *,
+        author.name as author_name
+    };
 }
--- srv/sales-service.js
+++ srv/sales-service.js
@@ -1,55 +1,66 @@
-const cds = require('@sap/cds')
-const { SELECT } = cds.ql
+const cds = require("@sap/cds");
 
-module.exports = class SalesService extends cds.ApplicationService { init() {
+module.exports = class SalesService extends cds.ApplicationService {
+  init() {
+    const { SalesOrders, SalesOrderItems, Books } = this.entities;
 
-  const { SalesOrders, SalesOrderItems } = this.entities
-
-  this.on('CREATE', SalesOrders, async (req) => {
-    const { data } = req
-    data.ID = cds.utils.uuid()
-    data.orderDate = new Date()
-    data.status = 'New'
-    return data
-  })
-
-  this.on('CREATE', SalesOrderItems, async (req) => {
-    const { data } = req
-    data.ID = cds.utils.uuid()
-    data.totalPrice = data.quantity * data.unitPrice
-
-    if (data.book_ID) {
-      const book = await SELECT.one.from('Books').where({ ID: data.book_ID })
-      if (book) {
-        data.productName = book.title
-        data.productCode = book.ID.toString()
-        data.unitPrice = book.price
-        data.currency = book.currency
-        data.totalPrice = data.quantity * book.price
+    // Auto-generate ID and set default values for new sales orders
+    this.before("CREATE", SalesOrders, async (req) => {
+      const { data } = req;
+      if (!data.ID) data.ID = cds.utils.uuid();
+      if (!data.orderDate) data.orderDate = new Date().toISOString().slice(0, 10);
+      if (!data.status) data.status = "NEW";
+      if (!data.orderNumber) {
+        data.orderNumber = `SO-${Math.floor(Math.random() * 90000 + 10000)}`;
       }
-    }
+    });
 
-    return data
-  })
-
-  this.on('UPDATE', SalesOrderItems, async (req) => {
-    const { data } = req
-
-    if (data.book_ID) {
-      const book = await SELECT.one.from('Books').where({ ID: data.book_ID })
-      if (book) {
-        data.productName = book.title
-        data.productCode = book.ID.toString()
-        data.unitPrice = book.price
-        data.currency = book.currency
+    // Auto-generate ID and calculate totals for new sales order items
+    this.before("CREATE", SalesOrderItems, async (req) => {
+      const { data } = req;
+      if (!data.ID) data.ID = cds.utils.uuid();
+      
+      // Calculate total price
+      if (data.quantity && data.unitPrice) {
+        data.totalPrice = Number((data.quantity * data.unitPrice).toFixed(2));
       }
-    }
+    });
 
-    if (data.quantity && data.unitPrice) {
-      data.totalPrice = data.quantity * data.unitPrice
-    }
-    return data
-  })
+    // Update totals when sales order items are modified
+    this.before("UPDATE", SalesOrderItems, async (req) => {
+      const { data } = req;
+      
+      // Recalculate total price if quantity or unit price changed
+      if (data.quantity && data.unitPrice) {
+        data.totalPrice = Number((data.quantity * data.unitPrice).toFixed(2));
+      }
+    });
 
-  return super.init()
-}}
+    // Update sales order total when items change
+    this.after("CREATE", SalesOrderItems, async (item) => {
+      await this.updateSalesOrderTotal(item.salesOrder_ID);
+    });
+
+    this.after("UPDATE", SalesOrderItems, async (item) => {
+      await this.updateSalesOrderTotal(item.salesOrder_ID);
+    });
+
+    this.after("DELETE", SalesOrderItems, async (item) => {
+      await this.updateSalesOrderTotal(item.salesOrder_ID);
+    });
+
+    // Delegate requests to the underlying generic service
+    return super.init();
+  }
+
+  // Helper method to update sales order total
+  async updateSalesOrderTotal(salesOrderId) {
+    const tx = cds.tx();
+    const items = await tx.read(SalesOrderItems).where({ salesOrder_ID: salesOrderId });
+    
+    if (items.length > 0) {
+      const totalAmount = items.reduce((sum, item) => sum + (item.totalPrice || 0), 0);
+      await tx.update(SalesOrders).where({ ID: salesOrderId }).with({ totalAmount });
+    }
+  }
+};
